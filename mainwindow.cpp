28aae3d85f5ad5962c83e1b5e5e7d0375eca099b
dronnikovigor <dronnikovigor@gmail.com>
2019-03-30 16:40:26 +0300

    Base project

---
+++ mainwindow.cpp
@@ -0,0 +1,13 @@
+#include "mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+{
+    this->setWindowTitle("Post Delivery Logistics");
+    this->setFixedSize(400,300);
+}
+
+MainWindow::~MainWindow()
+{
+
+}
